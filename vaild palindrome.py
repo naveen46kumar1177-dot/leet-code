49d81dbaa85fd45bcb38f0f710b1674e744347c3
NAVEENKUMAR.C <naveen46kumar1177@gmail.com>
2026-07-03 21:02:20 +0530

    Add files via upload

---
+++ vaild palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        char = ''
+        for i in s:
+            if i.isalnum():
+                char += i.lower()
+        return char==char[::-1]
